7b06dd84210d58a78163a46c63e908e7167c8af9
Peter Manoharan <peter.manoharan@gmail.com>
2025-10-21 06:10:00 +0100

    Initial Commit

---
+++ TallyTests/TallyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TallyTests.swift
+//  TallyTests
+//
+//  Created by Peter Manoharan on 21/10/2025.
+//
+
+import Testing
+@testable import Tally
+
+struct TallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
